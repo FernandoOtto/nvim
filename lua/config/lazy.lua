620f91dfc3da0669f4f37d7f87400795ea5c9f04
Fernando Otto <fernandootto369@gmail.com>
2025-10-13 11:51:39 -0300

    Adding colorschemes and modifying the plugins's struct

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,11 +25,11 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
   spec = {
     -- import your plugins
-    { import = "plugins" },
+    { import = "plugins/colorscheme" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
+  install = { colorscheme = { "hojicha" } },
   -- automatically check for plugin updates
   checker = { enabled = false },
 })
---
+++ lua/plugins/colorscheme/neomodern.lua
@@ -0,0 +1,11 @@
+return {
+	"cdmill/neomodern.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("neomodern").setup({
+      theme = "hojicha",
+    })
+		require("neomodern").load()
+	end,
+}
