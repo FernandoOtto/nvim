f407354dd53a24ea43a879b888baaeb41e8bc572
Fernando Otto <fernandootto369@gmail.com>
2025-10-13 12:03:55 -0300

    Adding 'oil' plugin

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -3,6 +3,7 @@ local wk = require("which-key")
 wk.add({
 	{
 		mode = { "n", "v" },
+    { "<leader>-", "<cmd>Oil<cr>", desc = "Open Oil" },
 		{ "<leader>q", "<cmd>q<cr>", desc = "Quit", hidden = true },
 		{ "<leader>w", "<cmd>w<cr>", desc = "Write", hidden = true },
 	},
