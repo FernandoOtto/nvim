465e0c2d3f2b8dd3494b1b5cfaa4a1c9faf1cadd
Fernando Otto <fernandootto369@gmail.com>
2025-10-13 11:28:53 -0300

    Add options

---
+++ lua/options.lua
@@ -0,0 +1,25 @@
+local cmd = vim.cmd
+local opt = vim.opt
+local o   = vim.o
+local g   = vim.g
+
+opt.shortmess:append("sI")
+opt.cmdheight = 0
+
+o.number = true
+o.relativenumber = true
+
+o.tabstop = 2
+o.shiftwidth = 2
+o.softtabstop = 2
+o.expandtab = true
+
+o.mouse = "a"
+
+o.termguicolors = true
+o.signcolumn = "yes"
+o.undofile = true
+o.wrap = false
+
+g.loaded_netrw = 1
+g.loaded_netrwPlugin = 1
