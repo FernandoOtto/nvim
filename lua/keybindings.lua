4878b0796adee14bb123b2741911c192323a11c8
Fernando Otto <fernandootto369@gmail.com>
2025-10-13 12:06:17 -0300

    Adding 'telescope' plugin

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -8,6 +8,13 @@ wk.add({
 		{ "<leader>w", "<cmd>w<cr>", desc = "Write", hidden = true },
 	},
 
+  -- Telescope
+	{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+	{ "<leader>fo", "<cmd>Telescope oldfiles<cr>", desc = "Help Tags" },
+	{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
+	{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find Buffers" },
+	{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
+
 	{
 		"<leader>?",
 		function()
